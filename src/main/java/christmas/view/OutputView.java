0c7f00d580ce4a388c070ba145a386fa779063fe
yyeennyy <kye3314@gmail.com>
2023-11-10 23:45:55 +0900

    feat: 이벤트 배지

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -96,7 +96,20 @@ public class OutputView {
             newLine();
             return;
         }
-        newLine();
+        int benefitPrice = benefits.getAllBenefitsPrice();
+        if (benefitPrice >= 20000) {
+            System.out.println("산타");
+            return;
+        }
+        if (benefitPrice >= 10000) {
+            System.out.println("트리");
+            return;
+        }
+        if (benefitPrice >= 5000) {
+            System.out.println("별");
+            return;
+        }
+        System.out.println("없음");
     }
 
     public void newLine() {
